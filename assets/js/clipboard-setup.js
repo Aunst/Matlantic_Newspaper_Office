598d7fa658a3aabe4ec4b942a990022b1f3c6a9e
Aunst <minecraft_aunst@163.com>
2022-09-03 18:21:36 +0800

    优化友情链接

---
+++ assets/js/clipboard-setup.js
@@ -0,0 +1,27 @@
+jQuery( document ).ready(function() {
+  var clipboard = new ClipboardJS( '.click_to_copy' );
+
+  jQuery( '.click_to_copy' ).attr( 'title', '复制成功!' );
+  jQuery( '.click_to_copy' ).tooltip({ trigger: 'manual' });
+
+  clipboard.on( 'success', function ( el ) {
+    console.info( 'Action:', el.action );
+    console.info( 'Text:', el.text );
+    console.info( 'Trigger:', el.trigger );
+
+    el.clearSelection();
+    jQuery( el.trigger ).tooltip( 'show' );
+
+    jQuery( el.trigger ).blur( function() {
+      jQuery( this ).tooltip( 'hide' );
+    });
+    jQuery( el.trigger ).mouseleave( function() {
+      jQuery( this ).tooltip( 'hide' );
+    });
+  });
+
+  clipboard.on( 'error', function ( el ) {
+    console.error('Action:', el.action);
+    console.error('Trigger:', el.trigger);
+  });
+});
