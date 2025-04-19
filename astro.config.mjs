2b50d59263e6f61d98d29e69bc229038cf13c51a
Aunst <Minecraft_Aunst@163.com>
2025-04-19 10:53:34 +0800

    尝试bugfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: USER_SITE,
-  base: "Matlantic-Newspaper-Office",
+  base: "Matlantic_Newspaper_Office",
   output: "static",
   style: {
     scss: {
